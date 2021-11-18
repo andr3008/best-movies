8de3c5b5e0b52bb4580ade28fe8848f8926bc25b
Korotenko Alex <alexey.korotenko90@gmail.com>
2021-11-18 12:58:55 +0200

    fetch+refs

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import './sass/main.scss';
 import cardTemp from './templates/cardTemplate.hbs';
 const refs = {
   cardContainer: document.querySelector('.collection'),
+  homeBtn: document.querySelector('.js-button__home'),
+  libraryBtn: document.querySelector('.js-button__library'),
+  watchedBtn: document.querySelector('.js-library__watched'),
+  queueBtn: document.querySelector('.js-library__queue'),
 };
+console.log(refs.queueBtn)
 fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=9aaadf590dc90bb88adc9b4200a95438')
   .then(response => {
     return response.json();
   })
   .then(item => {
     console.log(item);
-    const markup = cardTemp(item);
+    const markup = cardTemp(item.results);
     refs.cardContainer.innerHTML = markup;
   })
   .catch(err => {
