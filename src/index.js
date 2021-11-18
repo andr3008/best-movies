da4709e13213c307c73025b05ac937af9afe9256
Korotenko Alex <alexey.korotenko90@gmail.com>
2021-11-18 14:29:12 +0200

    header

--- src/index.js
+++ src/index.js
@@ -11,10 +11,13 @@ const refs = {
   cardContainer: document.querySelector('.collection'),
   homeBtn: document.querySelector('.js-button__home'),
   libraryBtn: document.querySelector('.js-button__library'),
+  libraryContainer:document.querySelector('.js-library'),
   watchedBtn: document.querySelector('.js-library__watched'),
   queueBtn: document.querySelector('.js-library__queue'),
+  searchForm:document.querySelector(".js-search__form"),
+  searchBtn:document.querySelector('.js-search__button'),
 };
-console.log(refs.queueBtn)
+// console.log(refs.libraryContainer);
 fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=9aaadf590dc90bb88adc9b4200a95438')
   .then(response => {
     return response.json();
@@ -28,3 +31,16 @@ fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=9aaadf590dc90bb8
     console.log(err);
   });
 
+refs.libraryBtn.addEventListener('click', onLibraryClick)
+refs.homeBtn.addEventListener('click', onHomeClick)
+
+
+function onLibraryClick() {
+  refs.libraryContainer.classList.remove('hide')
+  refs.searchForm.classList.add('hide')
+}
+
+function onHomeClick() {
+  refs.libraryContainer.classList.add('hide')
+  refs.searchForm.classList.remove('hide')
+}
