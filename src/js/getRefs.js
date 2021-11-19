b71296be1e589734d74793bee64da8b6905ce1d4
ZjeniiaSt <zeniiastupjak@gmail.com>
2021-11-19 08:42:40 +0300

    refs

---
+++ src/js/getRefs.js
@@ -0,0 +1,11 @@
+export default function getRefs() {
+  return {
+    homeBtn: document.querySelector('.js-button__home'),
+    libraryBtn: document.querySelector('.js-button__library'),
+    libraryContainer: document.querySelector('.js-library'),
+    watchedBtn: document.querySelector('.js-library__watched'),
+    queueBtn: document.querySelector('.js-library__queue'),
+    searchForm: document.querySelector('.js-search__form'),
+    searchBtn: document.querySelector('.js-search__button'),
+  };
+}
