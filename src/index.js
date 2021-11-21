9305c305cc86d2ce5df5ae3c0af891decb013300
ArtemKolesnyk <kolesnik2405@gmail.com>
2021-11-21 22:13:49 +0100

    add code auth to init-firebase

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import './sass/main.scss';
 import './js/changeTheme.js';
-
 import './js/fetchCard';
 import './js/headerListeners';
-
-
 import './js/fetchAPI';
 import './js/navigation';
 import './js/headerListeners';
 import './js/loader';
 // import './js/init-firebase';
+import './js/registration';
 import './js/logoAnime';
 import './js/dev-modal';
-
 import './js/pagination';
 import './js/btnUp';
-
-
 import './js/openMovieCard';
 
 
