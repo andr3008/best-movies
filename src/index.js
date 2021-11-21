49e3477b49b0e00c6f4fd07ddea300a69a37130d
ZjeniiaSt <zeniiastupjak@gmail.com>
2021-11-21 14:58:11 +0300

    -

--- src/index.js
+++ src/index.js
@@ -17,14 +17,6 @@ import './js/dev-modal';
 
 import './js/registration';
 import './js/init-firebase';
-const refs = {
-  cardContainer: document.querySelector('.collection'),
-  homeBtn: document.querySelector('.js-button__home'),
-  libraryBtn: document.querySelector('.js-button__library'),
-  watchedBtn: document.querySelector('.js-library__watched'),
-  queueBtn: document.querySelector('.js-library__queue'),
-};
-
 import './js/pagination';
 import './js/btnUp';
 
