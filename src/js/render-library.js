e069c91f2f2e5c0626d99c310bba0960c707a70e
Korotenko Alex <alexey.korotenko90@gmail.com>
2021-11-27 21:59:57 +0200

    local

--- src/js/render-library.js
+++ src/js/render-library.js
@@ -5,41 +5,52 @@ import cardTemp from '../templates/library-template.hbs';
 const newApi = new NewApi();
 const refs = getRefs();
 
-// refs.libraryBtn.addEventListener('click', renderWatchedContainer)
-// refs.watchedBtn.addEventListener('click',renderWatchedContainer)
-
-// function renderWatchedContainer() {
-//     const movies = JSON.parse(localStorage.getItem('moviesWatched'))
-//     return Promise.all(movies.map(id => newApi.fetchOneMovieInfo(id))).then(cardRender)
-// }
-
-// function cardRender(results) {
-//     refs.cardContainer.innerHTML = cardTemp(results)
-// }
 
 class LibraryPaint {
-  constructor() {}
+  constructor() { }
 
   eventListeners = () => {
-    refs.libraryBtn.addEventListener('click', this.renderWatchedContainer);
+    refs.libraryBtn.addEventListener('click', this.renderLibrary);
     refs.watchedBtn.addEventListener('click', this.renderWatchedContainer);
     refs.queueBtn.addEventListener('click', this.renderQueueContainer);
   };
+
+  renderLibrary = () => {
+    refs.paginationContainer.classList.add('hide');
+    refs.filter.classList.add('hide');
+    refs.propos.innerHTML = `<h2 class ='hidden__title'> Choose a section </h2>`;
+    refs.cardContainer.innerHTML = '';
+  }
 
   renderWatchedContainer = () => {
     refs.watchedBtn.classList.add('library__btn--active');
     refs.queueBtn.classList.remove('library__btn--active');
     refs.paginationContainer.classList.add('hide');
 
+    if (localStorage.getItem('moviesWatched') === null) {
+      refs.propos.innerHTML = `<h2 class ='hidden__title'> There are no films in the library </h2>`;
+      refs.cardContainer.innerHTML = ''
+      return
+    }
+    refs.propos.innerHTML = '';
     const movies = JSON.parse(localStorage.getItem('moviesWatched'));
     return Promise.all(movies.map(id => newApi.fetchOneMovieInfo(id))).then(this.cardRender);
   };
+
+  
 
   renderQueueContainer = () => {
     refs.queueBtn.classList.add('library__btn--active');
     refs.watchedBtn.classList.remove('library__btn--active');
     refs.paginationContainer.classList.add('hide');
 
+    if (localStorage.getItem('movieQueue') === null) {
+      refs.propos.innerHTML = `<h2 class ='hidden__title'> There are no films in the library </h2>`;
+      refs.cardContainer.innerHTML = ''
+      return
+    }
+
+    refs.propos.innerHTML = '';
     const movies = JSON.parse(localStorage.getItem('movieQueue'));
     return Promise.all(movies.map(id => newApi.fetchOneMovieInfo(id))).then(this.cardRender);
   };
