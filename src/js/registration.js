9f05e2ee2e6089c69923972b4ee7b85fbc6e589c
ArtemKolesnyk <kolesnik2405@gmail.com>
2021-11-28 21:58:07 +0100

    change class hide, remove scrol in modal

--- src/js/registration.js
+++ src/js/registration.js
@@ -1,19 +1,19 @@
-import { error, success, info } from '@pnotify/core';
-import '@pnotify/core/dist/PNotify';
-import '@pnotify/core/dist/BrightTheme.css';
+import { initializeApp } from 'firebase/app';
 import {
   getAuth,
+  onAuthStateChanged,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signInWithPopup,
   GoogleAuthProvider,
-  signOut,
-} from 'firebase/auth';
-
+  signOut, 
+}
+from 'firebase/auth';
+import '@pnotify/core/dist/PNotify';
+import '@pnotify/core/dist/BrightTheme.css';
+import { error, success, info } from '@pnotify/core';
 import getRefs from './get-refs';
-const refs = getRefs();
 
-import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDP-8fHTx4doMcXDAuOcqy68cGBn51fiy0',
@@ -24,7 +24,7 @@ const firebaseConfig = {
   messagingSenderId: '148206011547',
   appId: '1:148206011547:web:173c99d779451cd74bb23e',
 };
-
+const refs = getRefs();
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
@@ -39,8 +39,8 @@ refs.registerForm.addEventListener('sumbit', createNewUser);
 refs.showPassBtn.addEventListener('click', showPass);
 
 // создать акаунт
-function createNewUser() {
-  e = e.preventDefault();
+function createNewUser(e) {
+  e.preventDefault();
   let password = '';
   const email = e.target.email.value;
   console.log(email, password);
@@ -64,7 +64,7 @@ function createNewUser() {
       });
     })
     .then(() => {
-      refs.signUpModal.classList.add('is-hidden');
+      refs.signUpModal.classList.add('hide');
       e.target.email.value = null;
       e.target.pass.value = null;
       e.target.secondpass.value = null;
@@ -81,13 +81,39 @@ function createNewUser() {
     });
 }
 
+// получить текущего пользователя
+function getCurrentUser() {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      const uid = user.uid;
+      refs.signUpBtn.classList.add('hide');
+      refs.signInBtn.classList.add('hide');
+      refs.logOutBtn.classList.remove('hide');
+      success({
+        title: 'Success!',
+        text: 'You have successfully signed in.',
+        delay: 1000,
+      });
+      // ...
+    } else {
+      // User is signed out
+      refs.signUpBtn.classList.remove('hide');
+      refs.signInBtn.classList.remove('hide');
+      refs.logOutBtn.classList.add('hide');
+      
+    }
+  });
+}
+
+
 // вход с паролем
 
-function signinEmailAndPassword() {
-  e = e.preventDefault();
+function signinEmailAndPassword(e) {
+  e.preventDefault();
   const email = e.target.email.value;
   const password = e.target.pass.value;
-  refs.signinSpinner.classList.remove('is-hidden');
   signInWithEmailAndPassword(auth, email, password)
     .then(userCredential => {
       // Signed in
@@ -96,7 +122,7 @@ function signinEmailAndPassword() {
       // ...
     })
     .then(() => {
-      refs.signInModal.classList.add('is-hidden');
+      refs.signInModal.classList.add('hide');
       e.target.email.value = null;
       e.target.pass.value = null;
       window.location.reload(true);
@@ -109,7 +135,7 @@ function signinEmailAndPassword() {
         text: errorMessage,
         delay: 1000,
       }).finally(() => {
-        refs.loginForm.classList.remove('is-hidden');
+        refs.loginForm.classList.remove('hide');
       });
     });
 }
@@ -118,40 +144,40 @@ function signinEmailAndPassword() {
 
 function signInGoogleAcount() {
   signInWithPopup(auth, provider)
-    .then(result => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      if (user) {
-        refs.signUpBtn.classList.add('is-hidden');
-        refs.signInBtn.classList.add('is-hidden');
-        refs.logOutBtn.classList.remove('is-hidden');
-        window.location.reload();
-      }
-    })
-    .catch(error => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-    });
+  .then(result => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    if (user) {
+      refs.signUpBtn.classList.add('hide');
+      refs.signInBtn.classList.add('hide');
+      refs.logOutBtn.classList.remove('hide');
+      window.location.reload();
+    }
+  })
+  .catch(error => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    
+  });
 }
 
 function openSignUpModal() {
-  refs.signUpModal.classList.remove('is-hidden');
+  refs.signUpModal.classList.remove('hide');
   refs.signUpModal.addEventListener('click', hideSignUpModal);
   window.addEventListener('keydown', hideSignUpEsc);
   document.body.style.overflow = 'hidden';
 }
 
 function openSignInModal() {
-  refs.signInModal.classList.remove('is-hidden');
+  refs.signInModal.classList.remove('hide');
   document.body.style.overflow = 'hidden';
   refs.signInModal.addEventListener('click', hideSignInModal);
   window.addEventListener('keydown', hideSignInEsc);
@@ -166,8 +192,8 @@ function showPass() {
 }
 
 function signUpNow() {
-  refs.signInModal.classList.add('is-hidden');
-  refs.signUpModal.classList.remove('is-hidden');
+  refs.signInModal.classList.add('hide');
+  refs.signUpModal.classList.remove('hide');
   refs.signUpModal.addEventListener('click', hideSignUpModal);
   window.addEventListener('keydown', hideSignUpEsc);
 }
@@ -176,14 +202,14 @@ function signUpNow() {
 
 function hideSignInModal(e) {
   if (e.target === e.currentTarget) {
-    refs.signInModal.classList.add('is-hidden');
+    refs.signInModal.classList.add('hide');
     document.body.style.overflow = 'visible';
   }
 }
 
 function hideSignUpModal(e) {
   if (e.target === e.currentTarget) {
-    refs.signUpModal.classList.add('is-hidden');
+    refs.signUpModal.classList.add('hide');
     document.body.style.overflow = 'visible';
   }
 }
@@ -191,23 +217,27 @@ function hideSignUpModal(e) {
 function hideSignInEsc(e) {
   if (e.key === 'Escape') {
     document.body.style.overflow = 'visible';
-    refs.signInModal.classList.add('is-hidden');
+    refs.signInModal.classList.add('hide');
   }
 }
 
 function hideSignUpEsc(e) {
   if (e.key === 'Escape') {
     document.body.style.overflow = 'visible';
-    refs.signUpModal.classList.add('is-hidden');
+    refs.signUpModal.classList.add('hide');
   }
 }
 
 // выйти с акаунтa
 function LogOutFilmoteka() {
-  signOut(auth).then(() => {
+  signOut(auth)
+  .then(() => {
     info({
       text: 'You have been logged out.',
       delay: 1000,
     });
   });
 }
+
+
+
